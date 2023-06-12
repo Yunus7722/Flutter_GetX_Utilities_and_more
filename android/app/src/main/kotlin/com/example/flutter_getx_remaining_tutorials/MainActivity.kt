c6237b4101df09030b9b59442a602138068051cb
codeNurture023 <118299570+Yunus7722@users.noreply.github.com>
2023-06-12 13:47:30 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_getx_remaining_tutorials/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_getx_remaining_tutorials
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GetX Tutorial"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                
+                child: ListTile(
+                  title: Text("GetX 1"),
+                  subtitle: Text("Get X First Card"),
+                  onTap: (){
+                    Get.defaultDialog(
+                      title: "Dialog Box",
+                      middleText: "Are you sure u want to do it ? ",
+                      // textConfirm: "ok",
+                      // textCancel: "cancel"
+                      contentPadding: EdgeInsets.all(10),
+                      titlePadding: EdgeInsets.all(20),
+                      confirm: TextButton(onPressed: (){
+                        // Navigator.pop(context);
+                        Get.back();
+            
+                      }, child: Text("Ok")),
+                      cancel: TextButton(onPressed: (){}, child: Text("Cancel"))
+            
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                
+                child: ListTile(
+                  title: Text("GetX 2"),
+                  subtitle: Text("Get X Second Card"),
+                  onTap: (){
+                    Get.defaultDialog(
+                      title: "Dialog Box",
+                      middleText: "Are you sure u want to do it ? ",
+                      // textConfirm: "ok",
+                      // textCancel: "cancel"
+                      contentPadding: EdgeInsets.all(10),
+                      titlePadding: EdgeInsets.all(20),
+                      confirm: TextButton(onPressed: (){
+                        // Navigator.pop(context);
+                        Get.back();
+            
+                      }, child: Text("Ok")),
+                      cancel: TextButton(onPressed: (){}, child: Text("Cancel"))
+            
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Get.snackbar("Notification", "You are not here",
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.redAccent,
+        margin: EdgeInsets.all(20),
+        icon:Icon(Icons.add)
+        );
+      }),
+    );
+  }
+}
