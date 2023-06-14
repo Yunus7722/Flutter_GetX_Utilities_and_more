762fef8fed95efaf6e1d7e8f52e548ac06edbf85
codeNurture023 <118299570+Yunus7722@users.noreply.github.com>
2023-06-14 15:56:32 +0530

    Switch with Getx State management

---
+++ lib/controllers/switch_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class SwitchController extends GetxController{
+  
+  RxBool notification = false.obs;
+  
+  setNotification(bool newValue){
+    notification.value=newValue;
+  }
+}
--- lib/pages/switch_page.dart
+++ lib/pages/switch_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_getx_remaining_tutorials/controllers/switch_controller.dart';
+import 'package:get/get.dart';
 
 class SwitchPage extends StatefulWidget {
   const SwitchPage({super.key});
@@ -9,37 +11,45 @@ class SwitchPage extends StatefulWidget {
 }
 
 class _SwitchPageState extends State<SwitchPage> {
+  //dependency Injection
+  SwitchController controller = Get.put(SwitchController());
 
-  bool notification = false;
+  // bool notification = false;
 
   @override
   void initState() {
     super.initState();
-    
   }
 
   @override
   Widget build(BuildContext context) {
     print("build");
     return Scaffold(
-      appBar: AppBar(title: Text("Switch"),),
+      appBar: AppBar(
+        title: Text("Switch"),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Row(
           children: [
             Text("Notifications"),
             Expanded(child: Container()),
-            Switch(value: notification, onChanged: (value){
-              notification = value;
-              print(notification);
-              setState(() {
-                
-              });
+            Obx(() {
+              return Switch(
+                  value: controller.notification.value,
+                  onChanged: (value) {
+                    // notification = value;
+                    // print(notification);
+                    // setState(() {
+
+                    // });
+                    controller.setNotification(value);
+                    print(controller.notification.value);
+                  });
             })
           ],
         ),
       ),
-
     );
   }
 }
